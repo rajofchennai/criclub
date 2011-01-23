bbef0b990501ba4c4a3e1470a5e3f51da515d7b8
Rajesh Shanmugam <rajofchennai@yahoo.com>
2011-01-23 19:02:35 +0530

    removed layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,12 +7,22 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have title" do
+	get 'home'
+	response.should have_selector("title",:content => "Ruby on Rails Tutorial App | Home")	
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+	    it "should have title" do
+	get 'contact'
+	response.should have_selector("title",:content => "Ruby on Rails Tutorial App | Contact")	
     end
   end
   
@@ -21,6 +31,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+	    it "should have title" do
+	get 'about'
+	response.should have_selector("title",:content => "Ruby on Rails Tutorial App | about")	
+    end
   end
-	
+  
+  	
 end
