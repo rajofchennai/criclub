d884d130b0a1057768ccc693ff0bbcda4255d20c
Rajesh Shanmugam <rajofchennai@yahoo.com>
2011-01-23 19:00:14 +0530

    test continued

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  def contact
-  end
-
+def home
+end
+def contact
+end
+def about
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ FirstApp::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+	
+  get "pages/about"	
 
   resources :microposts
 
